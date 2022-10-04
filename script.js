e527126d7efd27f665a57e400c9301d8b123936a
Imran <imranmansuri038@gmail.com>
2022-10-05 02:08:00 +0530

    testimonials section added

--- script.js
+++ script.js
@@ -40,3 +40,24 @@ for (let i = 0; i < searchBoxElems.length; i++) {
     document.body.classList.toggle("active");
   });
 }
+
+// move cycle on scroll
+const deliveryBoy = document.querySelector(".delivery-img");
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+});
