92c13b78c8dae320a2b5556a145465e0491a03e3
Imran <imranmansuri038@gmail.com>
2022-10-06 01:08:51 +0530

    footer section added

--- script.js
+++ script.js
@@ -17,12 +17,15 @@ for (let i = 0; i < navbarLinks.length; i++) {
 
 // * header sticky & back to top
 const header = document.querySelector(".header");
+const backTopBtn = document.querySelector(".back-top-btn");
 
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 100) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 });
 
